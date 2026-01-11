4c2d61f02ac520123f0854b12d53f660c327fb34
Md. Abdullah All Shamem <abdullah107189@gmail.com>
2026-01-11 15:39:19 +0600

    update shop gift hero section

--- src/components/shop-gifts/ShopGiftsHero.tsx
+++ src/components/shop-gifts/ShopGiftsHero.tsx
@@ -4,7 +4,7 @@ import sidbar1 from "@/assets/banner_and_background/sidebar1.png";
 import sidbar2 from "@/assets/banner_and_background/sidebar2.png";
 const ShopGiftsHero = () => {
   return (
-    <section className="relative max-w-main md:mt-36 py-15    overflow-hidden">
+    <section className="relative max-w-main xl:mt-36 md:mt-30 mt-15 xl:py-15 md:py-10 py-5 overflow-hidden">
       <div>
         <img
           className="absolute w-[676.301px] h-[347.417px] rotate-[23.2deg] -top-50 -left-80"
@@ -19,19 +19,19 @@ const ShopGiftsHero = () => {
       </div>
 
       <div className="max-w-container mx-auto px-3">
-        <div className="flex items-center justify-between gap-12 lg:gap-18">
+        <div className="xl:flex items-center justify-between md:gap-12 xl:gap-18 text-center xl:text-start">
           {/* Left Side: Content Area */}
-          <div className="flex-1 flex flex-col items-start gap-4 order-2 lg:order-1">
-            <span className="text-gray-900 font-medium text-lg">
+          <div className="flex-1 flex flex-col xl:items-start items-center md:gap-4 gap-2 order-2 lg:order-1">
+            <span className="text-gray-900 font-medium md:text-lg">
               Shop Gifts
             </span>
 
-            <h1 className="text-5xl font-playfair md:text-[56px] font-bold text-gray-900 leading-[1.2]">
+            <h1 className="font-playfair xl:text-[56px] lg:text-5xl md:text-4xl text-3xl font-bold text-gray-900 leading-[1.2]">
               Find the Perfect Gift for <br className="hidden md:block" /> Every
               Moment
             </h1>
 
-            <p className="text-gray-700 text-lg md:text-2xl leading-relaxed max-w-xl">
+            <p className="text-gray-700 xl:text-2xl lg:text-xlmd:text-lg text-base leading-relaxed max-w-xl">
               Browse thoughtfully curated gift categories made for every
               occasion, every milestone, and every special person in your life.
             </p>
@@ -42,7 +42,7 @@ const ShopGiftsHero = () => {
           </div>
 
           {/* Right Side: Illustration Area */}
-          <div className="flex-1 w-full order-1 lg:order-2">
+          <div className="flex-1 hidden xl:flex w-full order-1 lg:order-2">
             <div className=" max-w-[600px] object-contain relative flex justify-center lg:justify-end">
               <img
                 src={shopHeroImg}
