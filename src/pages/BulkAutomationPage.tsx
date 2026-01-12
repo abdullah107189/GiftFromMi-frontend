c777033e4fa4e29f2952b038f830c1bd10629375
Md. Abdullah All Shamem <abdullah107189@gmail.com>
2026-01-12 09:16:18 +0600

    create bulk operation section

--- src/pages/BulkAutomationPage.tsx
+++ src/pages/BulkAutomationPage.tsx
@@ -1,9 +1,11 @@
 import BulkAutomationHero from "@/components/bulk-automation/BulkAutomationHero";
+import BulkOperationsSection from "@/components/bulk-automation/BulkOperationsSection";
 
 function BulkAutomationPage() {
   return (
     <div>
       <BulkAutomationHero></BulkAutomationHero>
+      <BulkOperationsSection></BulkOperationsSection>
     </div>
   );
 }
