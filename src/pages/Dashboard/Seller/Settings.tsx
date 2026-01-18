3365e7b569c30a3f6e2693b61259b31ecec45772
abdullah107189 <abdullah107189@gmail.com>
2026-01-18 18:07:43 +0600

    create settings pages and complete header and tabs

--- src/pages/Dashboard/Seller/Settings.tsx
+++ src/pages/Dashboard/Seller/Settings.tsx
@@ -1,8 +1,13 @@
- 
+import { ProfileTabs } from "@/components/Dashboard/Seller/Settings/ProfileTabs";
+import SettingsHeader from "@/components/Dashboard/Seller/Settings/SettingsHeader";
+
 function Settings() {
   return (
-    <div>Settings</div>
-  )
+    <div>
+      <SettingsHeader></SettingsHeader>
+      <ProfileTabs></ProfileTabs>
+    </div>
+  );
 }
 
-export default Settings
+export default Settings;
