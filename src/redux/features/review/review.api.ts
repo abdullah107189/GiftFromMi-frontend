4783ec85e2d0f14a585aa1cb8d60e708fa1161d8
Md. Abdullah All Shamem <abdullah107189@gmail.com>
2026-03-14 17:22:50 +0600

    Create myReviews, createReview, updateReview, deleteReview

--- src/redux/features/review/review.api.ts
+++ src/redux/features/review/review.api.ts
@@ -7,10 +7,23 @@ const reviewApi = baseApi.injectEndpoints({
             providesTags: ["REVIEWS"],
             transformResponse: (res) => res,
         }),
+        myReviews: builder.query({
+            query: () => ({ url: `/review/my-reviews`, method: "GET" }),
+            providesTags: ["REVIEWS"],
+            transformResponse: (res) => res,
+        }),
         createReview: builder.mutation({
             query: (data) => ({ url: "/review/store", method: "POST", data }),
             transformResponse: (res) => res,
-        })
+        }),
+        updateReview: builder.mutation({
+            query: ({ id, data }) => ({ url: `/review/update/${id}`, method: "PATCH", data }),
+            transformResponse: (res) => res,
+        }),
+        deleteReview: builder.mutation({
+            query: ({ id }) => ({ url: `/review/destroy/${id}`, method: "DELETE" }),
+            transformResponse: (res) => res,
+        }),
     }),
 })
-export const { useAllReviewsQuery } = reviewApi;
+export const { useAllReviewsQuery, useCreateReviewMutation, useUpdateReviewMutation } = reviewApi;
