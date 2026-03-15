fe134453871293c447e98adeccb0911b177f66b6
Md. Abdullah All Shamem <abdullah107189@gmail.com>
2026-03-15 10:09:04 +0600

    Create set up user profile api

--- src/redux/features/user/user.api.ts
+++ src/redux/features/user/user.api.ts
@@ -8,6 +8,48 @@ export const adminApi = baseApi.injectEndpoints({
       providesTags: ["USERS"],
       transformResponse: (res) => res,
     }),
+
+    getSingleUser: builder.query({
+      query: () => ({ url: "/user-detail", method: "GET" }),
+      providesTags: ["USERS"],
+      transformResponse: (res) => res,
+    }),
+
+    userChangePassword: builder.mutation({
+      query: (credentials) => ({
+        url: "/change-password",
+        method: "POST",
+        data: credentials,
+      }),
+      invalidatesTags: ["USERS"],
+    }),
+
+    userProfileUpdate: builder.mutation({
+      query: (credentials) => ({
+        url: "/profile/update",
+        method: "POST",
+        data: credentials,
+      }),
+      invalidatesTags: ["USERS"],
+    }),
+
+    userAvaterUpload: builder.mutation({
+      query: (credentials) => ({
+        url: "/profile-avatar-upload",
+        method: "POST",
+        data: credentials,
+      }),
+      invalidatesTags: ["USERS"],
+    }),
+    
+    userAvaterRemove: builder.mutation({
+      query: (credentials) => ({
+        url: "/profile-avatar-remove",
+        method: "POST",
+        data: credentials,
+      }),
+      invalidatesTags: ["USERS"],
+    })
   }),
 });
 export const { useGetAllUsersQuery } = adminApi;
